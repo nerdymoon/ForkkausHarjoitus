be64d083dbbed3a18c2527860779fce7085008c3
nerdymoon <90307132+nerdymoon@users.noreply.github.com>
2021-09-22 09:59:17 +0300

    Add code
    Lisättiin Java MOOC-tehtävä Gitiin

---
+++ osa01-Osa01_16.SekunnitVuorokaudessa/src/main/java/SekunnitVuorokaudessa.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class SekunnitVuorokaudessa {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+        // toteuta ohjelma tänne
+        System.out.println("Kuinka monen vuorokauden sekunnit tulostetaan?");
+        int lkm = Integer.valueOf(lukija.nextLine());
+        int sekunnitVrk = ((60*60)*24)*lkm;
+        System.out.println(sekunnitVrk);
+
+    }
+}
